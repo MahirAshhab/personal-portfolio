a5e889cc052c924ccbccc678616351d1ea86406b
MahirAshhab <mahir.ashhab77@gmail.com>
2022-02-14 03:12:20 +0600

    Navbar and Hero Section

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { ImFacebook2, ImLinkedin, ImGithub } from 'react-icons/im';
+import './hero.css';
+
+const Hero = () => {
+    return (
+        <div className="
+        hero-container
+        grid grid-cols-2 gap-x-10
+        items-center
+        pr-28 pl-28 pt-10 pb-10">
+            <div className="
+            hero-header 
+            flex flex-col 
+            justify-center">
+                <h1 className="
+                text-8xl font-bold text-left">
+                    Hello,
+                    <br />
+                    I,m Mahir
+                    <br />
+                    Ashhab
+                </h1>
+            </div>
+            <div className="hero-para">
+                <p className="
+                text-justify text-sm font-light">
+                    A Sydney based frontend designer, working and living in Australia. I love to work on anything code, from designing simple HTML pages to solving complex problems. I love to illustrate as well. Recetly I won the award for best portfolio. Download the CV to know more about my background.
+                </p>
+            </div>
+            <div className="
+            pt-6 
+            flex">
+                <button className="
+                cv 
+                p-4 pl-8 pr-8 
+                text-lg">
+                    Download CV
+                </button>
+            </div>
+            <div className="
+            icon-container
+            pt-6 
+            flex">
+                <a
+                    href="https://www.facebook.com/mahir.ashhab.71/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="
+                    pr-6">
+                    <ImFacebook2 className="text-2xl" />
+                </a>
+                <a
+                    href="https://github.com/MahirAshhab/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="
+                    pr-6">
+                    <ImGithub className="text-2xl" />
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/mahir-ashhab77/"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <ImLinkedin className="text-2xl" />
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Hero from '../Hero/Hero';
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul className="
+            flex 
+            justify-end items-end 
+            text-base font-bold 
+            pr-28 pl-28 pt-6 pb-6
+            bg-orange-100">
+                <li className="mr-8">Education</li>
+                <li className="mr-8">Projects</li>
+                <li>About</li>
+            </ul>
+            <Hero></Hero>
+        </div>
+    );
+};
+
+export default Navbar;
